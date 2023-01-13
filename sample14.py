da7027aae000f32852979c3687d5b9151a79a0c3
Orelso Vitam <orelso.vitam@avanio.com>
2023-01-13 16:31:12 +0200

    update

---
+++ sample14.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+
+if name in ["Huey", "Dewey", "Louie"]:
+    print("You are one of Donald Duck's nephews.")
+elif name in ["Morty", "Ferdie"]:
+    print("You are one of Mickey Mouse's nephews.")
+else:
+    print("You're not a nephew of any character I know of.")
