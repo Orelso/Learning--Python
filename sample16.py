87fb5d303b11ca8f82ce233fe9b54999467b4567
Orelso Vitam <orelso.vitam@avanio.com>
2023-01-16 14:19:41 +0200

    update

---
+++ sample16.py
@@ -0,0 +1,7 @@
+letter1 = input("1st letter:")
+letter2 = input("2nd letter:")
+letter3 = input("3rd letter:")
+
+letters = [letter1 , letter2, letter3]
+letters.sort()
+print(f"The letter in the middle is {letters[1]}")
